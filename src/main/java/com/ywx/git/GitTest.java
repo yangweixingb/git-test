0954a2ec510dbe72305f38a148898118f16b6155
ywx <498524357@qq.com>
2022-06-19 13:32:45 +0800

    second commit

--- src/main/java/com/ywx/git/GitTest.java
+++ src/main/java/com/ywx/git/GitTest.java
@@ -7,5 +7,6 @@ package com.ywx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
